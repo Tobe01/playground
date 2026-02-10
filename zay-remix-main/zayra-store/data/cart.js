d2635337f576733189fc44df3146f3d1f55392d4
Tobechi Duru, B.Eng <tobechi.duru001@gmail.com>
2026-02-10 15:52:26 -0800

    updates!

---
+++ zay-remix-main/zayra-store/data/cart.js
@@ -0,0 +1,10 @@
+
+
+export function Cart(){
+  const cartItems = [{
+    Image: '2149684322.webp',
+    itemDescription: 'Slim Stacked Flare Embroidered Strip Tricot Track Pants',
+    priceCents: 2230,
+    quantity: 1
+  }]
+}
